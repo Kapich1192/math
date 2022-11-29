fd19cee12e5fb9e2616fd707c0026f82d4d48fda
barbreye <barbreye@student.21-school.ru>
2022-11-29 16:46:48 +0300

    develop --> barbreye --> developing

--- src/common/s21_fabs.c
+++ src/common/s21_fabs.c
@@ -5,8 +5,8 @@
  * */
 long double s21_fabs(double x) {
   long double result = x;
-  if(s21_isnan(x)) {
-      result = x;
+  if (s21_isnan(x)) {
+    result = x;
   } else if (x > 0) {
     result = x;
   } else if (x < 0) {
