afa2237660c2f7702a5a00b10a10bdc5bfead9c2
barbreye <barbreye@student.21-school.ru>
2022-11-30 14:35:52 +0300

    develop --> barbreye --> developing

--- src/tests/s21_sqrt_tests/s21_sqrt_tests.c
+++ src/tests/s21_sqrt_tests/s21_sqrt_tests.c
@@ -20,56 +20,67 @@ START_TEST(s21_sqrt_4) {
 }
 END_TEST
 
-START_TEST(s21_sqrt_5){
-    ck_assert_double_eq(s21_sqrt(0),sqrt(0));
-}END_TEST
+START_TEST(s21_sqrt_5) { ck_assert_double_eq(s21_sqrt(0), sqrt(0)); }
+END_TEST
 
-START_TEST(s21_sqrt_6){
-    int t = 0;
-    if(isnan(sqrt(-1)) && s21_isnan(s21_sqrt(-1))) t = 1;
-    ck_assert_int_eq(1,t);
-}END_TEST
+START_TEST(s21_sqrt_6) {
+  int t = 0;
+  if (isnan(sqrt(-1)) && s21_isnan(s21_sqrt(-1))) t = 1;
+  ck_assert_int_eq(1, t);
+}
+END_TEST
 
-START_TEST(s21_sqrt_7){
-    ck_assert_ldouble_eq_tol(s21_sqrt(0.123123123123), sqrt(0.123123123123),S21_EPS);
-}END_TEST
+START_TEST(s21_sqrt_7) {
+  ck_assert_ldouble_eq_tol(s21_sqrt(0.123123123123), sqrt(0.123123123123),
+                           S21_EPS);
+}
+END_TEST
 
 START_TEST(s21_sqrt_8) {
-    ck_assert_ldouble_eq_tol(s21_sqrt(0.999999999),sqrt(0.999999999),S21_EPS);
-} END_TEST
+  ck_assert_ldouble_eq_tol(s21_sqrt(0.999999999), sqrt(0.999999999), S21_EPS);
+}
+END_TEST
 
 START_TEST(s21_sqrt_9) {
-    ck_assert_ldouble_infinite(s21_sqrt(INFINITY));
-    ck_assert_ldouble_infinite(sqrt(INFINITY));
-} END_TEST
+  ck_assert_ldouble_infinite(s21_sqrt(INFINITY));
+  ck_assert_ldouble_infinite(sqrt(INFINITY));
+}
+END_TEST
 
 START_TEST(s21_sqrt_10) {
-    ck_assert_ldouble_nan(s21_sqrt(-INFINITY));
-    ck_assert_ldouble_nan(sqrt(-INFINITY));
-} END_TEST
+  ck_assert_ldouble_nan(s21_sqrt(-INFINITY));
+  ck_assert_ldouble_nan(sqrt(-INFINITY));
+}
+END_TEST
 
 START_TEST(s21_sqrt_11) {
-    ck_assert_ldouble_nan(s21_sqrt(-7));
-    ck_assert_ldouble_nan(sqrt(-7));
-} END_TEST
+  ck_assert_ldouble_nan(s21_sqrt(-7));
+  ck_assert_ldouble_nan(sqrt(-7));
+}
+END_TEST
 
 START_TEST(s21_sqrt_12) {
-    ck_assert_ldouble_nan(s21_sqrt(NAN));
-    ck_assert_ldouble_nan(sqrt(NAN));
-} END_TEST
+  ck_assert_ldouble_nan(s21_sqrt(NAN));
+  ck_assert_ldouble_nan(sqrt(NAN));
+}
+END_TEST
 
 START_TEST(s21_sqrt_13) {
-    ck_assert_ldouble_eq_tol(s21_sqrt(159159159159.159159159),sqrt(159159159159.159159159),S21_EPS);
-} END_TEST
+  ck_assert_ldouble_eq_tol(s21_sqrt(159159159159.159159159),
+                           sqrt(159159159159.159159159), S21_EPS);
+}
+END_TEST
 
 START_TEST(s21_sqrt_14) {
-    ck_assert_ldouble_nan(s21_sqrt(-159159159159.159159159));
-    ck_assert_ldouble_nan(sqrt(-159159159159.159159159));
-} END_TEST
+  ck_assert_ldouble_nan(s21_sqrt(-159159159159.159159159));
+  ck_assert_ldouble_nan(sqrt(-159159159159.159159159));
+}
+END_TEST
 
 START_TEST(s21_sqrt_15) {
-    ck_assert_ldouble_eq_tol(s21_sqrt(17.01),sqrt(17.01), S21_EPS);
-} END_TEST
+  ck_assert_ldouble_eq_tol(s21_sqrt(17.01), sqrt(17.01), S21_EPS);
+}
+END_TEST
 
 Suite* s21_sqrt_suite_create(void) {
   Suite* suite = suite_create("s21_sqrt");
