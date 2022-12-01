b941a7e1b1e576769022ea35993db37b56c87d5e
barbreye <barbreye@student.21-school.ru>
2022-12-01 17:37:29 +0300

    develop --> barbreye --> developing

--- src/tests/main.c
+++ src/tests/main.c
@@ -6,36 +6,36 @@
 
 int main(void) {
   int temp = 0;
-    /*#1 s21_abs*/
-    temp = test_abs();
-    /*#2 s21_acos*/
-    temp = test_acos();
-    /*#3 s21_asin*/
-    temp = test_asin();
-    /*#4 s21_atan*/
-    temp = test_atan();
-    /*#5 s21_ceil*/
-    temp = test_ceil();
-    /*#6 s21_cos*/
-    temp = test_cos();
-    /*#7 s21_exp*/
-    temp = test_exp();
-    /*#8 s21_fabs*/
-    temp = test_fabs();
-    /*#9 s21_floor*/
-    temp = test_floor();
-    /*#10 s21_fmod*/
-    temp = test_fmod();
-      /*#11 s21_log*/
-      temp = test_log();
+  /*#1 s21_abs*/
+  temp = test_abs();
+  /*#2 s21_acos*/
+  temp = test_acos();
+  /*#3 s21_asin*/
+  temp = test_asin();
+  /*#4 s21_atan*/
+  temp = test_atan();
+  /*#5 s21_ceil*/
+  temp = test_ceil();
+  /*#6 s21_cos*/
+  temp = test_cos();
+  /*#7 s21_exp*/
+  temp = test_exp();
+  /*#8 s21_fabs*/
+  temp = test_fabs();
+  /*#9 s21_floor*/
+  temp = test_floor();
+  /*#10 s21_fmod*/
+  temp = test_fmod();
+  /*#11 s21_log*/
+  temp = test_log();
   /*#12 s21_pow*/
   temp = test_pow();
-    /*#13 s21_sin*/
-    temp = test_sin();
-    /*#14 s21_sqrt*/
-    temp = test_sqrt();
-    /*#15 s21_tan*/
-    temp = test_tan();
+  /*#13 s21_sin*/
+  temp = test_sin();
+  /*#14 s21_sqrt*/
+  temp = test_sqrt();
+  /*#15 s21_tan*/
+  temp = test_tan();
   temp = 0;
   return temp;
 }
